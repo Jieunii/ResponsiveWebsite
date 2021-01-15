5fa3bb6bf6feb9bcf63db01e790bd05a48348104
Jieunii <yjelbf@naver.com>
2021-01-15 19:45:21 +0900

    Add scroll smooth and header for mobile

--- custom.js
+++ custom.js
@@ -1,5 +1,5 @@
 $(function(){
-    /* Trigger */
+    // Trigger
     $('.trigger').click(function(){
         $(this).toggleClass('active')
         $('.gnb').toggleClass('active')
@@ -9,8 +9,18 @@ $(function(){
         $('.trigger').removeClass('active')
     })
 
-    /* Smooth Scrolling */
-    $('.menu a').click(function(e){
+    // Smooth Scrolling
+    $('.menu a, .gototop').click(function(e){
         $.scrollTo(this.hash || 0, 900)
+    })
+
+    // Change CSS with Scroll
+    $(window).scroll(function(){
+        if($(window).scrollTop() > 50) {
+            $('header, .gototop').addClass('active')
+        }
+        else {
+            $('header, .gototop').removeClass('active')
+        }
     })
 })
